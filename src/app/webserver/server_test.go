36d58be1730d7217b162e6579ef7ca2e35c982d2
moura1001 <gmouraneto01@gmail.com>
2022-08-11 09:50:05 -0300

    commit com refatoração nos testes do servidor, extraindo o store stub para fácil reutilização em outros pacotes

--- src/app/webserver/server_test.go
+++ src/app/webserver/server_test.go
@@ -10,24 +10,6 @@ import (
 	"testing"
 )
 
-type StubGameStore struct {
-	likes     map[string]int
-	likeCalls []string
-	polling   model.Polling
-}
-
-func (s *StubGameStore) GetGameLikes(name string) int {
-	return s.likes[name]
-}
-
-func (s *StubGameStore) RecordLike(name string) {
-	s.likeCalls = append(s.likeCalls, name)
-}
-
-func (s *StubGameStore) GetPolling() model.Polling {
-	return s.polling
-}
-
 func TestGETLikes(t *testing.T) {
 	st := StubGameStore{
 		map[string]int{
