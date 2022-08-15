a84a1a3a3e2b0f5f576c1a28c05185c4f7513650
moura1001 <gmouraneto01@gmail.com>
2022-08-15 15:52:04 -0300

    commit com implementação do agendamento de alertas e integração na aplicação CLI

--- src/app/alerter/alerter.go
+++ src/app/alerter/alerter.go
@@ -1,7 +1,23 @@
 package alerter
 
-import "time"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 type BlindAlerter interface {
 	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+type BlindAlerterFunc func(duration time.Duration, amount int)
+
+func (alerter BlindAlerterFunc) ScheduleAlertAt(duration time.Duration, amount int) {
+	alerter(duration, amount)
+}
+
+func StdOutAlerter(duration time.Duration, amount int) {
+	time.AfterFunc(duration, func() {
+		fmt.Fprintf(os.Stdout, "Blind is now %d\n", amount)
+	})
 }
--- src/app/cmd/cli/main.go
+++ src/app/cmd/cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"moura1001/mega_like_x/src/app/alerter"
 	"moura1001/mega_like_x/src/app/cli"
 	"moura1001/mega_like_x/src/app/store"
 	"os"
@@ -10,9 +11,12 @@ import (
 
 func main() {
 	fmt.Println("Where's the floor?")
-	fmt.Println("Type '{Game} like' to record a vote")
+	fmt.Println("Type '{Game} wins' to record a win")
 
-	poll, err := cli.NewCLI(store.IN_MEMORY, os.Stdin, nil)
+	poll, err := cli.NewCLI(
+		store.IN_MEMORY, os.Stdin, nil,
+		alerter.BlindAlerterFunc(alerter.StdOutAlerter),
+	)
 
 	if err != nil {
 		log.Fatalf("poll initialization error: %v", err)
